b12ed3781b3a5a3795be4c75acae8d4e1302e735
miruy <dbflarla4966@naver.com>
2022-03-11 17:38:29 +0900

    delete MVCExam4

--- MVCexam4/src/main/java/spring/AlreadyExistingMemberException.java
+++
@@ -1,9 +0,0 @@
-package spring;
-
-public class AlreadyExistingMemberException extends RuntimeException{
-	//멤버 등록 시 이메일을 검색하여 있는 이메일인지 확인 
-	//있는 이메일이라면 표시할 예외처리 생성 
-	public AlreadyExistingMemberException(String message) {
-		super(message);
-	}
-}
--- MVCexam4/src/main/java/spring/MemberNotFoundException.java
+++
@@ -1,6 +0,0 @@
-package spring;
-
-public class MemberNotFoundException extends RuntimeException{
-	//비밀번호 변경 전 이메일로 기존회원이 맞는지 확인할 때
-	//존재하지 않는 이메일이라는 예외처리 생성 
-}
