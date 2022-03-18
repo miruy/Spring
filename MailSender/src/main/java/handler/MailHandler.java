d9bc9b8822cdc6a377e49165734a3345e41013e8
miruy <dbflarla4966@naver.com>
2022-03-18 18:26:15 +0900

    Project MailSender Update

---
+++ MailSender/src/main/java/handler/MailHandler.java
@@ -0,0 +1,15 @@
+package handler;
+
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+public class MailHandler {
+	private JavaMailSender mailSender;
+	private MimeMessage message;
+	private MimeMessageHelper messageHelper;
+	
+	
+	
+}
